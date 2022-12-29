bd0691d952411eb1c852e157aea810fb338e5dd3
Md.khosruzzaman <khosru.skz@gmail.com>
2022-12-29 22:18:34 +0000

    first commit

---
+++ besic.js
@@ -0,0 +1,66 @@
+//variable
+
+var deposit = 200;
+
+//condition
+
+if (deposit > 500) {
+    
+
+}
+
+if else(deposit < 500) {
+ 
+    
+}
+
+else (deposit <= 500) {
+    
+
+}
+
+
+//array
+
+const numbers = [45, 6587, 124, 1, 365];
+const NumberCount = numbers.length;  // variable.length to use for find the array length.
+numbers.pop();  //use for remove the last number of array
+numbers.push(); // add value last in array
+numbers[2] = 555;
+
+//check weather 222 included in the array
+
+if (numbers.includes(222) != -1) {
+    
+
+}
+if (numbers.includes(222){
+
+
+})
+
+//loop
+//while 
+
+for (const number of numbers) {
+    
+
+}
+
+//function
+
+function fullName(firstName, secondName) {
+    
+    const name = first + '' +  second;
+
+}
+
+const person = fullName('rahim ', ' Ali ');
+
+//object
+
+const bottle = { color : 'yoellow ' , contain : 'water ' }
+
+
+
+
